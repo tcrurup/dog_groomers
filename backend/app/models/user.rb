573fb4cbaebcb8d0cf48131e4b47de66fe7212bb
tcrurup <tcrurup@gmail.com>
2019-10-29 00:31:19 -0700

    add validations to user model for presence of first and last name as well as a unique username

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,16 +1,30 @@
 class User < ActiveRecord::Base
 
-    #username   -This Users chosen username
-    #first_name -This Users first name
-    #last_name  -This Users last name
+    #username (string)   -This Users chosen username
+    #first_name (string) -This Users first name
+    #last_name (string)  -This Users last name
     
     #ASSOCIATIONS
     has_many :user_dogs
     has_many :dogs, 
         through: :user_dogs
 
+    #VALIDATIONS
+    validates :username,
+        presence: true,
+        uniqueness: {
+            message: " is already in user by another member"
+        }
+
+    validates :first_name,
+        presence: true
+
+    validates :last_name,
+        presence: true   
+    
     #FUNCTIONS
     def create_dog(dog_params={})
+        #Creates a new Dog object with given params and associates it with this User
         Dog.create(dog_params).tap do |x|
             self.dogs << x
         end
