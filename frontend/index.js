b35f6a4f646058cb1b32489a79fd123d8503caec
tcrurup <tcrurup@gmail.com>
2019-10-29 20:07:22 -0700

    create FormBuilder class to make creating forms in the DOM much easier

--- frontend/index.js
+++ frontend/index.js
@@ -5,7 +5,42 @@ document.addEventListener('DOMContentLoaded', function(){
     fetch(LOGIN_URL)
     .then( response => response.json() )
     .then( object => {
-        console.log(object)
+        showLogInPage()
     })
     .catch( error => console.log(error) );
 });
+
+function showLogInPage(){
+    let mainNode = document.querySelector('main');
+    let form = new FormBuilder(LOGIN_URL, "POST")
+    form.addInputElement('text', 'user_name')
+    form.addInputElement('password', 'user_password')
+    mainNode.appendChild(form.finalize())
+}
+
+class FormBuilder{
+
+    constructor(action, method){
+        let element = document.createElement("form") 
+        element.action = action
+        element.method = method
+        this.form = element
+    }
+
+    addInputElement(type, name){
+        let element = document.createElement("input")
+        element.type = type
+        element.id = name
+        element.name = name
+        this.form.appendChild(element)
+    }
+
+    finalize(){
+        let element = document.createElement('button')
+        element.type = 'submit'
+        element.innerHTML = 'Submit'
+        this.form.appendChild(element)
+        return this.form
+    }
+}
+
