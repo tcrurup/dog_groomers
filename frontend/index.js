338293f3643eac174a75e8f979031b3131af0a7c
tcrurup <tcrurup@gmail.com>
2019-10-29 22:29:32 -0700

    change styling and add FormBuilder class to assist in building forms for application

--- frontend/index.js
+++ frontend/index.js
@@ -13,8 +13,8 @@ document.addEventListener('DOMContentLoaded', function(){
 function showLogInPage(){
     let mainNode = document.querySelector('main');
     let form = new FormBuilder(LOGIN_URL, "POST")
-    form.addInputElement('text', 'user_name')
-    form.addInputElement('password', 'user_password')
+    form.addInputElement('text', 'username')
+    form.addInputElement('password', 'password')
     mainNode.appendChild(form.finalize())
 }
 
@@ -24,15 +24,35 @@ class FormBuilder{
         let element = document.createElement("form") 
         element.action = action
         element.method = method
+
+        let table = document.createElement("table")
+        element.appendChild(table)
+        
         this.form = element
+        this.form_table = table
     }
 
     addInputElement(type, name){
-        let element = document.createElement("input")
-        element.type = type
-        element.id = name
-        element.name = name
-        this.form.appendChild(element)
+        let form_row = document.createElement('tr')
+        let form_header = document.createElement('th')
+        let form_data = document.createElement('td')
+
+        let label = document.createElement("label")
+            label.for = name
+            label.innerHTML = `${FormBuilder.capitalize(name)}: `
+
+        let input = document.createElement("input")
+            input.type = type
+            input.id = name
+            input.name = name
+
+        form_header.appendChild(label)
+        form_data.appendChild(input)
+
+        form_row.appendChild(form_header)
+        form_row.appendChild(form_data)
+
+        this.form_table.appendChild(form_row)
     }
 
     finalize(){
@@ -42,5 +62,11 @@ class FormBuilder{
         this.form.appendChild(element)
         return this.form
     }
+
+    static capitalize(string){
+        return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+    }
+
+    
 }
 
