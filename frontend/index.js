8b1b7aff9c7a5c173c60ad0ba190f390f11a32f5
tcrurup <tcrurup@gmail.com>
2019-10-30 01:48:49 -0700

    create LinkBuiler class in javascript and updata FormBuilder for clarity

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,6 @@
 const BASE_URL = "http://localhost:3000"
 const LOGIN_URL = `${BASE_URL}/login`
+const SIGNUP_URL = `${BASE_URL}/signup`
 
 document.addEventListener('DOMContentLoaded', function(){
     fetch(LOGIN_URL)
@@ -15,11 +16,25 @@ function showLogInPage(){
     let form = new FormBuilder(LOGIN_URL, "POST")
     form.addInputElement('text', 'username')
     form.addInputElement('password', 'password')
+    form.addStringBreak(`Don't have an account? ${LinkBuilder.hyperlink_to(SIGNUP_URL, 'Sign up!')}`)
     mainNode.appendChild(form.finalize())
+}
+
+
+class LinkBuilder{
+
+    constructor(url){
+        this.url = url
+    }
+
+    static hyperlink_to(url, text){
+        return `<a href=${url}>${text}</a>`
+    }
 }
 
 class FormBuilder{
 
+    
     constructor(action, method){
         let element = document.createElement("form") 
         element.action = action
@@ -28,31 +43,53 @@ class FormBuilder{
         let table = document.createElement("table")
         element.appendChild(table)
         
-        this.form = element
-        this.form_table = table
+        this.form = element;
+        this.form_table = table;
+        return this
     }
+
+
 
     addInputElement(type, name){
         let form_row = document.createElement('tr')
         let form_header = document.createElement('th')
         let form_data = document.createElement('td')
+        
 
+        form_header.appendChild(this.createFormLabel(name))
+        form_data.appendChild(this.createFormInput(type, name))
+        
+        let row_data = [form_header, form_data]
+        row_data.forEach( element =>{ form_row.appendChild(element) })
+        
+        this.form_table.appendChild(form_row)
+    }
+
+    addStringBreak(string){
+        let form_row = document.createElement('tr')
+        let form_data = document.createElement('td')
+
+        form_data.colspan = 2;
+        form_data.innerHTML = string
+
+        form_row.appendChild(form_data)
+        
+        this.form_table.appendChild(form_row)
+    }
+
+    createFormLabel(name){
         let label = document.createElement("label")
             label.for = name
             label.innerHTML = `${FormBuilder.capitalize(name)}: `
+        return label
+    }
 
+    createFormInput(type, name){
         let input = document.createElement("input")
             input.type = type
             input.id = name
             input.name = name
-
-        form_header.appendChild(label)
-        form_data.appendChild(input)
-
-        form_row.appendChild(form_header)
-        form_row.appendChild(form_data)
-
-        this.form_table.appendChild(form_row)
+        return input
     }
 
     finalize(){
@@ -65,8 +102,6 @@ class FormBuilder{
 
     static capitalize(string){
         return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
-    }
-
-    
+    }    
 }
 
